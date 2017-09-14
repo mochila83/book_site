c86f254b02f8c0309a9e02c6698693fc6e248793
Jessica OBrien <obrien.jessica6@gmail.com>
2017-09-14 16:33:09 -0400

    more styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,20 @@ import AllAuthors from "./components/AllAuthors";
 import Author from "./components/Author";
 import "./App.css";
 import GlobalNav from "./components/GlobalNav";
+import styled from 'styled-components';
+
+
+const BookClub = styled.div`
+background-color: rgba(255, 255, 255, 0.5);
+a {
+  font-size: 30px;
+  text-decoration: none;
+  color: black;
+}
+a:hover {
+  background-color: blue;
+}
+`
 
 class App extends Component {
   render() {
@@ -12,10 +26,13 @@ class App extends Component {
         <div className="App">
           <div>
             <GlobalNav />
-            <h1>Book Club</h1>
+
+            <BookClub>
+            <h1>Welcome To The Book Club!</h1>
             <div>
               <Link to="/">Authors</Link>
             </div>
+            </BookClub>
           </div>
           <Route exact path="/" component={AllAuthors} />
           <Route path="/authors/:id" component={Author} />
